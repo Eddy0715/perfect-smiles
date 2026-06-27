6fe52dd804a7ccf5e24306739b09fd17188b5c35
Eddy <eddy@example.com>
2026-06-27 23:35:19 +0530

    Revert "Delete cases page, remove references from index.html navigation and vite config"
    This reverts commit 414d11ff36f442057178b197a8182268930343d0.

--- dr-abin-website/vite.config.js
+++ dr-abin-website/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        cases: resolve(__dirname, 'cases.html'),
       },
     },
   },
